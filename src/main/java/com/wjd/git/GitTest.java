ba4af7eb4209ff2bdd3d915cafa3f695869f4a16
Forgethson <649343453@qq.com>
2023-02-19 21:55:36 +0800

    hot-fix commit1

--- src/main/java/com/wjd/git/GitTest.java
+++ src/main/java/com/wjd/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("Version: 2");
         System.out.println("Version: 3");
+        System.out.println("Version: 4 hot-fix");
     }
 }
