3a3f5e9a99802b20a88dd9f80304171db5891073
Forgethson <649343453@qq.com>
2023-02-19 21:41:26 +0800

    first commit

---
+++ src/main/java/com/wjd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wjd.git;
+
+/**
+ * @author Jianda Wang
+ * @version 1.0
+ * 2023/2/19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
