1d0b18bd33f5c639740e36c2fad05a3a862d5faa
Forgethson <649343453@qq.com>
2023-02-19 21:43:36 +0800

    third commit

--- src/main/java/com/wjd/git/GitTest.java
+++ src/main/java/com/wjd/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("Version: 2");
+        System.out.println("Version: 3");
     }
 }
