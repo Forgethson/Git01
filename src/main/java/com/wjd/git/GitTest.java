e63a66ca95a8f2051b556c2f98ba1f717f130693
Forgethson <649343453@qq.com>
2023-02-19 21:43:04 +0800

    second commit

--- src/main/java/com/wjd/git/GitTest.java
+++ src/main/java/com/wjd/git/GitTest.java
@@ -8,5 +8,6 @@ package com.wjd.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("Version: 2");
     }
 }
