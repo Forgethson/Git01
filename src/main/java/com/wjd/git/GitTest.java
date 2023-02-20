2c2b818ff0244b8e4e5eba52c896c4cd84f88ffe
Forgethson <88615997+Forgethson@users.noreply.github.com>
2023-02-20 14:41:53 +0800

    GitHub:2023/2/20

--- src/main/java/com/wjd/git/GitTest.java
+++ src/main/java/com/wjd/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("---push---");
         System.out.println("---pull---");
+        System.out.println("---GitHub:2023/2/20---");
     }
 }
